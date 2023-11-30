bbd7755d38cd0e89cf82c12ad0cea7c5a892518e
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-30 18:33:13 +0530

    Working on OS Module

---
+++ script.js
@@ -0,0 +1,12 @@
+const os=require("os");
+const express=require("express");
+const cluster=require("cluster");
+
+let app=express();
+
+// console.log(os.cpus().length);
+if(cluster.isPrimary){
+    for(let i=0; i<os.cpus().length; i++){
+        cluster.fork();
+    }
+}
