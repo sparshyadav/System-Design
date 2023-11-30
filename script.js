9aa234d61ad6c66009afad08d980dc38f02611b6
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-30 18:33:35 +0530

    Updated and Formatted Code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const os=require("os");
-const express=require("express");
-const cluster=require("cluster");
+const os = require("os");
+const express = require("express");
+const cluster = require("cluster");
 
-let app=express();
+let app = express();
 
 // console.log(os.cpus().length);
-if(cluster.isPrimary){
-    for(let i=0; i<os.cpus().length; i++){
+if (cluster.isPrimary) {
+    for (let i = 0; i < os.cpus().length; i++) {
         cluster.fork();
     }
 }
